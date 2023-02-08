8b88ba84a830a798912d3112f10f7f33f01af81e
Djletun <djletun@tut.by>
2023-02-08 21:12:44 +0300

    добавил выборку серийников фдушек в считыватель

--- read_result_data.py
+++ read_result_data.py
@@ -2,6 +2,11 @@
 with open('filename.txt') as fl:
     dat = fl.readlines()
     fl.close()
+fdu_sn=list()
 for i in dat:
-    print(i)
+    #print(i)
+    if i.find('NODE')>0 and i.find('sn:')>0:
+        fdu_sn.append(i.split()[8])
+        print(fdu_sn[-1])
 print(len(dat))
+print(fdu_sn[5])
